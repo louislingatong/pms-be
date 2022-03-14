1c353d257f1230f9d008cfbbdd8c9c93dd6b7d79
Louis Lingatong <louis.li@digitalroominc.com>
2022-03-15 00:43:28 +0800

    Add params vessel to works counter

--- routes/api/works.php
+++ routes/api/works.php
@@ -2,17 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\VesselMachinerySubCategory;
-use Illuminate\Http\Request ;
+use App\Http\Requests\CountWorksRequest ;
 
 Route::group(['middleware' => ['auth:api']], function () {
     Route::get('/', 'WorkController@index');
     Route::post('/', 'WorkController@create');
 
-    Route::get('/count', function (Request $request) {
+    Route::get('/count', function (CountWorksRequest $request) {
+        $vessel = $request->getVessel();
         $work = [];
-        $work['warning'] = VesselMachinerySubCategory::searchByStatus(config('work.statuses.warning'))->count();
-        $work['due'] = VesselMachinerySubCategory::searchByStatus(config('work.statuses.due'))->count();
-        $work['overdue'] = VesselMachinerySubCategory::searchByStatus(config('work.statuses.overdue'))->count();
+        $work['warning'] = VesselMachinerySubCategory::searchByStatus(config('work.statuses.warning'))
+            ->whereHas('vesselMachinery.vessel', function ($q) use ($vessel) {
+                $q->where('name', '=', $vessel);
+            })
+            ->count();
+        $work['due'] = VesselMachinerySubCategory::searchByStatus(config('work.statuses.due'))
+            ->whereHas('vesselMachinery.vessel', function ($q) use ($vessel) {
+                $q->where('name', '=', $vessel);
+            })
+            ->count();
+        $work['overdue'] = VesselMachinerySubCategory::searchByStatus(config('work.statuses.overdue'))
+            ->whereHas('vesselMachinery.vessel', function ($q) use ($vessel) {
+                $q->where('name', '=', $vessel);
+            })
+            ->count();
         return response()->json(['data' => $work]);
     });
 });
